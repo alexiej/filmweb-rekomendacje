714ff6e3be16044e642ff44941a2a1fffcce932e
klemenko <arek.klemenko@gmail.com>
2019-11-27 08:40:53 +0100

    fix server issues after merge

--- server/server.py
+++ server/server.py
@@ -5,7 +5,7 @@ from flask import Flask, request, render_template
 import json
 
 from filmweb_integrator.fwimdbmerge.merger import Merger
-from movies_analyzer.data_provider import flow_chart_data, pie_chart_data, radar_chart_data
+from movies_analyzer.data_provider import records_data, flow_chart_data, pie_chart_data, radar_chart_data
 
 app = Flask(__name__, template_folder='templates')
 
@@ -19,17 +19,17 @@ def initialize():
 @app.route('/ping')
 def ping():
     return 'Hello world!'
+    
 
 
 @app.route('/render', methods=['GET', 'POST'])
 def render():
     if 'dane' in request.form:
         dane = json.loads(request.form['dane'])
-
         df = Merger(dane).get_data()
 
         return render_template("index.html",
-                               dane = df.fillna('').to_dict(),
+                               dane = dane,
                                flow = flow_chart_data(df),
                                radar = radar_chart_data(df),
                                pie = pie_chart_data(df))
