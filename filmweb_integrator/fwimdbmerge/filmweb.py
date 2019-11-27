d09d5edfff7ea8b37bcb1b176b3bb91ccd9a83f4
Mateusz Rusin <mateusz.rusin@pearson.com>
2019-11-27 19:18:08 +0100

    Fixes in integrator & analyzer

--- filmweb_integrator/fwimdbmerge/filmweb.py
+++ filmweb_integrator/fwimdbmerge/filmweb.py
@@ -8,55 +8,59 @@ from filmweb_integrator.fwapi.film import Film
 from .utils import to_list
 from pathlib import Path
 
+FILMWEB_DATA_MAPPING = {'id':'ID', 'tytułpolski':'Tytuł polski', 'tytułoryginalny':'Tytuł oryginalny',
+                        'rokprodukcji':'Rok produkcji', 'ulubione':'Ulubione', 'ocena':'Ocena',
+                        'komentarz': 'Komentarz', 'krajprodukcji':'Kraj produkcji', 'gatunek':'Gatunek', 'data':'Data'}
 
-FILMWEB_DATA_COLUMNS = ['ID', 'Tytuł polski', 'Tytuł oryginalny', 'Rok produkcji',
-                       'Ulubione', 'Ocena', 'Komentarz', 'Kraj produkcji', 'Gatunek', 'Data']
 ROOT = str(Path(__file__).parent.parent.parent.absolute().resolve())
 
-
 class Filmweb(object):
+    def __init__(self, df):
+        self.df = df.rename(columns=FILMWEB_DATA_MAPPING)
 
-    def get_dataframe(self, df, use_saved_scraped=False):
-        df.columns = FILMWEB_DATA_COLUMNS
+    def get_dataframe(self, use_saved_scraped=False):
+        df = self.df
         df = df.drop(columns=['Komentarz'])
         df = df[df.Ocena != 'brak oceny']
-        df['Ulubione'] = self._label_encode(df.Ulubione.fillna(''))
+        df['Ulubione'] = self.label_encode(df.Ulubione.fillna(''))
         df['Ocena'] = df.Ocena.astype(int)
 
         df = df.reset_index(drop=True)
-        df = df.join(self._dummies(df['Gatunek']), how='outer')
-        df = df.join(self._dummies(df['Kraj produkcji']), how='outer')
-        df = df.join(self._get_scrapped(df, use_saved_scraped), how='outer')
+        df = pd.concat([
+            df,
+            self.dummies(df['Gatunek'].fillna('')),
+            self.dummies(df['Kraj produkcji'].fillna('')),
+            self.get_scrapped(df, use_saved_scraped)], axis=1)
 
-        df['group'] = self._make_groups(df)
-        df['budget'] = self._fill_mean(df, 'budget')
-        df['boxoffice'] = self._fill_mean(df, 'boxoffice')
+        df['group'] = self.make_groups(df)
+        df['budget'] = self.fill_mean(df, 'budget')
+        df['boxoffice'] = self.fill_mean(df, 'boxoffice')
 
         return df.drop(columns=['group'])
 
-    def _get_scrapped(self, df, use_saved_scraped):
+    def get_scrapped(self, df, use_saved_scraped):
         if not use_saved_scraped:
             # warning - takes long time (a lot filmweb api calls)
             new_columns = ['budget', 'boxoffice', 'topics_count']
 
             scrapped = pd.DataFrame(columns=new_columns)
-            scrapped[new_columns] = df.apply(lambda x: self._movie_info(int(x.ID)), axis=1, result_type='expand')
+            scrapped[new_columns] = df.apply(lambda x: self.movie_info(int(x.ID)), axis=1, result_type='expand')
             scrapped[new_columns] = scrapped[new_columns].apply(lambda x: x.fillna(x.mean()), axis=0).astype(int)
             return scrapped
         else:
             return pd.read_csv(ROOT + '/data_static/oceny_scraped.csv')
             #  'https://raw.githubusercontent.com/mateuszrusin/ml-filmweb-score/dw-poznan-project/oceny_scraped.csv')
 
-    def _dummies(self, series):
+    def dummies(self, series):
         data = pd.get_dummies(series.apply(to_list).apply(pd.Series).stack()).sum(level=0)
         return data
 
-    def _label_encode(self, series):
+    def label_encode(self, series):
         encoder = LabelEncoder()
         encoder.fit(series)
         return encoder.transform(series)
 
-    def _movie_info(self, id):
+    def movie_info(self, id):
         try:
             film = Film.get_by_id(id)
             film.populate()
@@ -64,10 +68,9 @@ class Filmweb(object):
         except Exception:
             return None, None, None
 
-    @staticmethod
-    def _make_groups(df):
+    def make_groups(self, df):
         minidf = df.drop(
-            columns=['budget', 'boxoffice', 'ID', 'Gatunek', 'Kraj produkcji', 'Tytuł polski', 'Tytuł oryginalny', 'Data',
+            columns=['ID', 'Gatunek', 'Kraj produkcji', 'Tytuł polski', 'Tytuł oryginalny', 'Data',
                      'Ulubione', 'Rok produkcji'])
         minidf = minidf.fillna(0)
         kmeans = KMeans(n_jobs=-1, n_clusters=5)
@@ -75,5 +78,5 @@ class Filmweb(object):
         return kmeans.predict(minidf)
 
     @staticmethod
-    def _fill_mean(df, column):
+    def fill_mean(df, column):
         return df.groupby('group')[column].apply(lambda x: x.fillna(x.mean())).astype(int)
